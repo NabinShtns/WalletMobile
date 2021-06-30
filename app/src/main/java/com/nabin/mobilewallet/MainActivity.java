742346d5f2f2d94631b52c56632285e9df30f6a6
NabinShtns <nabin.sht.ns@gmail.com>
2021-06-30 15:24:37 +0545

    recycler view

--- app/src/main/java/com/nabin/mobilewallet/MainActivity.java
+++ app/src/main/java/com/nabin/mobilewallet/MainActivity.java
@@ -1,14 +1,42 @@
 package com.nabin.mobilewallet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.nabin.mobilewallet.MyAdapter.MyAdapter;
+import com.nabin.mobilewallet.model.moodel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+private RecyclerView recyclerview;
+private TextView name , location;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name =findViewById(R.id.Name);
+        location= findViewById(R.id.Location);
+
+        recyclerview = findViewById(R.id.recyclerview);
+        List<moodel> moodelList = new ArrayList<>();
+        moodelList.add(new moodel("nabin","thimi"));
+        moodelList.add(new moodel("mohan","balkumari"));
+        moodelList.add(new moodel("ramu","kathmandu"));
+        moodelList.add(new moodel("prabin","chapagaon"));
+        moodelList.add(new moodel("munna","bhaktapur"));
+
+        MyAdapter myAdapter = new MyAdapter(this, moodelList);
+        recyclerview.setAdapter(myAdapter);
+        recyclerview.setLayoutManager(new LinearLayoutManager(this));
+
+
+
     }
 }
--- app/src/main/java/com/nabin/mobilewallet/MyAdapter/MyAdapter.java
+++ app/src/main/java/com/nabin/mobilewallet/MyAdapter/MyAdapter.java
@@ -1,4 +1,62 @@
 package com.nabin.mobilewallet.MyAdapter;
 
-public class MyAdapter {
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.nabin.mobilewallet.R;
+import com.nabin.mobilewallet.model.moodel;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    Context mContext;
+    List<moodel> moodellist;
+    public MyAdapter(Context mContext, List<moodel> moodelList) {
+        this.mContext = mContext;
+        this.moodellist = moodelList;
+    }
+
+
+
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main,parent,false);
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+       moodel moodel = moodellist.get(position);
+       holder.tvname.setText(moodel.getName());
+       holder.tvlocation.setText(moodel.getLocation());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return moodellist.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvname, tvlocation;
+        public ViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+
+            tvname= itemView.findViewById(R.id.Name);
+            tvlocation= itemView.findViewById(R.id.Location);
+        }
+    }
 }
